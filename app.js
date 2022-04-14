c857f72346a18c3f211dbf5452cd88b3c8e4b08d
Anitha <anitha2591998@gmail.com>
2022-04-14 20:02:11 +0530

    Added cors options

--- app.js
+++ app.js
@@ -12,9 +12,13 @@ const hostname = 'localhost';
 // const locID = 'mongodb://127.0.0.1:27017/zomato_re';
 const serverDB = 'mongodb+srv://zomato_res:anitha2591998@cluster0.oijcg.mongodb.net/zomato_re?retryWrites=true&w=majority';
 
-
+const corsOpts = {
+    origin: '*',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type']
+};
  
-app.use(cors());
+app.use(cors(corsOpts));
 app.use(express.json());
 app.use('/', router);
 
